2d7f28fe8d5209e17020adde629750bd70d765af
milanwinter <milanwinter8@gmail.com>
2020-10-29 20:33:45 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def song_format
+    return "#{self.artist_name} - #{self.title}"
+  end
 end
